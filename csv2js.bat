22c1dee38184722100fcddd2b43e3f00225c0ac6
wfzirk <42407520+wfzirk@users.noreply.github.com>
2021-08-09 11:07:09 -0500

    921_8_5  fixed

--- csv2js.bat
+++ csv2js.bat
@@ -7,8 +7,8 @@ echo Create mfxxx.csv for madefrom tools
 
 
 
-set xrefin=xref921_8_5.1_EN.csv
-set outfile=mf921_8_5.1_EN.csv
+set xrefin=xref921_8_5_EN.csv
+set outfile=mf921_8_5_EN.csv
 
 cmd /c python csv2js.py %xrefin%
 
